9fc0c337b2ade9e17e9a69d596e4234d8b8e6fdf
Bhagyeshyeole <bhagyeshyeole24@gmail.com>
2025-07-04 23:40:40 +0530

    Create SUM.c

---
+++ SUM.c
@@ -0,0 +1,12 @@
+//Program to calculate sum of given numbers.
+#include <stdio.h>
+
+int main() 
+{
+    int number1 = 10;
+    int number2 = 15;
+    int sum = number1 + number2;
+
+    printf("The sum is: %d\n", sum);
+    return 0;
+}
