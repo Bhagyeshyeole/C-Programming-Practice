845a3e07d0d01ad8d975461d6ebbdbc57f028a10
Bhagyeshyeole <bhagyeshyeole24@gmail.com>
2025-07-09 21:55:07 +0530

    Update Marksheet.c

--- Marksheet.c
+++ Marksheet.c
@@ -8,17 +8,17 @@ main()
     float per;
     char name[20];
 
-    printf("Enter Roll No: ");
-    scanf("%d", &rlno);
-    printf("Enter Name of student: ");
-    scanf("%s", &name);
-    printf("Enter marks of three subject: ");
-    scanf("%d %d %d", &s1, &s2, &s3);
-    tot = s1 + s2 + s3;     
-    per=tot/3;
+        printf("Enter Roll No: ");
+        scanf("%d", &rlno);
+        printf("Enter Name of student: ");
+        scanf("%s", &name);
+        printf("Enter marks of three subject: ");
+        scanf("%d %d %d", &s1, &s2, &s3);
+            tot = s1 + s2 + s3;     
+            per=tot/3;
 
-    printf("\n Total: %d", tot);
-    printf("\n Percentage: %f", per);
+       printf("\n Total: %d", tot);
+       printf("\n Percentage: %f", per);
 
     return 0;
 }   
