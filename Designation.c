b994d438a81698605c23cc35c9bcab1f35208ae6
Bhagyeshyeole <bhagyeshyeole24@gmail.com>
2025-07-11 21:13:21 +0530

    Add files via upload

---
+++ Designation.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char code;
+    printf("Designation character \n");
+    printf("\n Manager = m ");
+    printf("\n Supervisior = s");
+    printf("\n Clerk = c ");
+    printf("\n Worker = w ");
+
+    printf("\n Enter the designation code: ");
+    scanf(" %c", &code);
+
+    if(code=='m')
+    {
+        printf("\n Manager");
+    }
+    else if(code=='s')
+    {
+        printf("\n Supervisior");
+    }
+    else if(code=='c')
+    {
+        printf("\n Clerk");
+    }
+    else if(code=='w')
+    {
+        printf("\n Worker");
+    }
+    else
+    {
+        printf("\n Invalid designation Chracter");
+    }
+    return 0;
+}   
