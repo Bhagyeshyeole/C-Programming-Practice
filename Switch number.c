f8f278c3a2f39c98359f2718a6e9e75065520e98
Bhagyeshyeole <bhagyeshyeole24@gmail.com>
2025-07-12 20:13:19 +0530

    Add files via upload

---
+++ Switch number.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int no;
+    printf("Enter a number 1-10 \n");
+    scanf("%d", &no);
+    switch(no)
+    {
+        case 1:
+            printf("ONE \n");
+            break;
+        case 2:
+            printf("TWO \n");
+            break;
+        case 3:
+            printf("THREE \n");
+            break;              
+        case 4:
+            printf("FOUR \n");
+            break;
+        case 5:
+            printf("FIVE \n");
+            break;              
+        case 6: 
+            printf("SIX \n");
+            break;          
+        case 7: 
+            printf("SEVEN \n");
+            break;  
+        case 8:
+            printf("EIGHT \n");
+            break;
+        case 9:
+            printf("NINE \n");
+            break;
+        case 10:
+            printf("TEN \n");
+            break;      
+        default:
+            printf("INVALID NUMBER \n");
+            break;
+    }
+    return 0;
+}
